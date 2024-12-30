9a7d41173d772f245b1810e4423ae430908e0208
StrigZ <mirgasan200@gmail.com>
2024-12-30 08:58:02 +0300

    Setup eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }
+  },
   pluginJs.configs.recommended,
-];
+  {
+    ignores: ['node_modules', 'dist']
+  },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended
+]
